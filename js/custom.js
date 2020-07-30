c145a9fef2af0332a35104c1b0f9dc151e8ea898
huynhquynh-dev <huynhquynh.data@gmail.com>
2020-07-30 14:03:45 +0700

    Update isotope menu

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,19 @@
-﻿ $(function(){
- 
-})  
- 
+﻿$(function () {
+    var groupMenu = $('.group-menu-home').isotope({
+        itemSelector: '.one-food',
+        layoutMode: 'masonry'
+    });
+    // layout Isotope after each image loads
+    groupMenu.imagesLoaded().progress(function () {
+        groupMenu.isotope('layout');
+    });
+    $('ul.menu-title-list li').click(function (event) {
+        event.preventDefault();
+        const type = $(this).data('food');
+        groupMenu.isotope({ filter: type });
+
+        // Active class menu title 
+        $('ul.menu-title-list li').removeClass('active');
+        $(this).addClass('active');
+    });
+});
